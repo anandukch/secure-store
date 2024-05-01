f5c2c9bf6425fd4a8d000449df79d4f97259732e
anandukch <anandukch@gmail.com>
2024-05-02 00:23:25 +0530

    adding functionality to show popup

--- extension/background.ts
+++ extension/background.ts
@@ -24,44 +24,134 @@ async function handleMessage({ action }: Message, response: ResponseCallback) {
     }
 }
 
+// const removePopupIfExists = async () => {
+//     try {
+//         const windows = await browser.windows.getAll({ populate: true });
+
+//         const existingPopup = windows.find((window) => window.type === "popup");
+
+//         if (existingPopup) {
+//             await browser.windows.remove(existingPopup.id!);
+//         }
+//     } catch (error) {
+//         return;
+//     }
+// };
+
+// const createWindow = () => {
+//      browser.windows.create({
+//         url: "index.html",
+//         type: "popup",
+//         width: 500,
+//         height: 500,
+//         top: 30,
+//         left: 30,
+//     });
+// }
+
+interface GlobalState {
+    url: string ;
+}
+
+let globalState : GlobalState;
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 
-browser.runtime.onMessage.addListener((msg, sender, response) => {
+browser.runtime.onMessage.addListener(async (msg, sender, response) => {
     handleMessage(msg, response);
 
-    if(msg.action === "pop"){
+    // browser.windows.getAll({populate:true}).then((windows) => {
+    //     console.log("windows", windows);
+
+    //     const existingPopup = windows.find((window) => window.type === "popup");
+    //     // console.log("existingPopup", existingPopup);
+
+    //     // // If an existing popup window is found, close it
+    //     if (existingPopup) {
+    //         browser.windows.remove(existingPopup.id!);
+    //     }
+    // });
+    // if (msg.action != "login") {
+    // await removePopupIfExists();
+    // }
+    if(msg.action === "mount"){
+        console.log("mounting");
+        console.log(msg,globalState);
+        
+       if(msg.payload.url === globalState.url){
+              console.log("same url");
+       }
+    }
+    // if (msg.action === "show_popup") {
+    //     browser.windows.create({
+    //         url: "index.html",
+    //         type: "popup",
+    //         width: 500,
+    //         height: 500,
+    //         top: 30,
+    //         left: 30,
+    //     });
+    // }
+
+    if(msg.action === "pageRefreshed"){
+        console.log("page refreshed");
+    }
+    if (msg.action === "pop") {
         browser.windows.create({
             url: "index.html",
             type: "popup",
             width: 500,
             height: 500,
-            top: 300,
+            top: 30,
+            left: 30,
         });
     }
+    if (msg.action === "login") {
+        // send reposne back
 
-    if(msg.action === "login"){
-        console.log(msg.data);
-    }
+        console.log("login",msg.payload);
 
-    browser.windows.getAll({populate:true}).then((windows) => {
-        console.log("windows", windows);
-        
-        // const existingPopup = windows.find((window) => window.type === "popup");
-        // console.log("existingPopup", existingPopup);
-        
-        // // If an existing popup window is found, close it
-        // if (existingPopup) {
-        //     browser.windows.remove(existingPopup.id!);
-        // }
-        // browser.windows.create({
-        //     url: "https://www.google.com/",
-        //     type: "popup",
-        //     width: 500,
-        //     height: 500,
-        //     top: 300,
+        // createWindow();
+        // browser.runtime.sendMessage({ action: "login", data: msg.payload });
+
+
+        globalState = {
+            url: msg.payload.url
+        }
+        // browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+        //     console.log("tabs", tabs);
+        //     browser.tabs.sendMessage(tabs[0].id!, { action: "login", data: msg.payload });
         // });
-    });
+
+        // browser.windows.getAll({ populate: true }).then((windows) => {
+        //     console.log("windows", windows);
+
+        //     const existingPopup = windows.find((window) => window.type === "popup");
+        //     // console.log("existingPopup", existingPopup);
+
+        //     // // If an existing popup window is found, close it
+        //     if (existingPopup) {
+        //         browser.windows.remove(existingPopup.id!);
+        //     }
+        //     browser.windows.create({
+        //         url: "index.html",
+        //         type: "popup",
+        //         width: 500,
+        //         height: 500,
+        //         top: 30,
+        //         left: 30,
+        //     });
+        // });
+    }
+    if (msg.action === "form_interaction") {
+        console.log(msg);
+    }
 
     // browser.runtime.sendMessage({ action: "show_popup" })
     return true;
 });
+
+// onready function);
+// browser.runtime.onInstalled.addListener(() => {
+//     removePopupIfExists();
+// });
--- extension/content-script/App.tsx
+++ extension/content-script/App.tsx
@@ -8,14 +8,21 @@ import React, { useEffect } from "react";
 function App() {
     useEffect(() => {
         console.log("App mounted");
+
+        document.addEventListener('DOMContentLoaded', function() {
+            console.log("DOM content loaded");
+            
+            browser.runtime.sendMessage({ action: 'pageRefreshed' });
+        });
+        browser.runtime.sendMessage({ action: "mount", payload: { url: window.location.href } });
         if (window.location.href.includes("login")) {
             // browser.runtime.sendMessage({ action: 'login page detected' });
             // find the username and password fields and fill them in
-            console.log("Filling in login form");
+            // console.log("Filling in login form");
 
-            const usernameField = document.querySelector('input[name="username"]') as HTMLInputElement;
-            const passwordField = document.querySelector('input[name="password"]') as HTMLInputElement;
-            console.log(usernameField, passwordField);
+            // const usernameField = document.querySelector('input[name="username"]') as HTMLInputElement;
+            // const passwordField = document.querySelector('input[name="password"]') as HTMLInputElement;
+            // console.log(usernameField, passwordField);
 
             // inert the username and password into the fields
             document.querySelector('input[name="username"]')?.setAttribute("value", "username");
@@ -27,27 +34,69 @@ function App() {
         }
 
         const handleUserInteraction = (event) => {
+            const fieldInfo = {
+                type: event.target.tagName,
+                value: event.target.value,
+                action: event.type,
+            };
+
+            if ((fieldInfo.type === "BUTTON" || fieldInfo.value === "Login") && fieldInfo.action === "click") {
+                // console.log("Login button clicked");
+                // can we get the resposne of the fetch request here
+                browser.runtime.sendMessage({
+                    action: "login",
+                    payload: {
+                        fieldInfo,
+                        url: window.location.href,
+                    },
+                });
+            }
+            // what to know if its a form submit event when a button is clicked
+            // console.log(event.target.tagName);
+
+            // Check if the target element is an input field or textarea
+
+            // if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+            //     const fieldInfo = {
+            //         type: event.target.tagName,
+            //         value: event.target.value,
+            //         action: event.type, // 'keydown', 'keyup', 'click', 'focus', 'change', etc.
+            //     };
+            //     // check if it is a login button
+
+            //     console.log("Field information:", fieldInfo);
+
+            //     // Send message to background script with field information
+            //     browser.runtime.sendMessage({ action: "form_interaction", payload: fieldInfo }).then((response) => {
+            //         console.log("Response from background script:", response);
+            //     });
+            // }
+
+            // check if it is a login button
+
+            // console.log("Field information:", fieldInfo);
+            browser.runtime.sendMessage({ action: "form_interaction", payload: event });
             // if (event.target.tagName === "BUTTON") {
             //     console.log("Login button clicked");
-                browser.runtime.sendMessage({ action: "login", data: event });
+            // browser.runtime.sendMessage({ action: "login", data: event });
             // }
 
             // Check if the target element is an input field or textarea
-            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
-                const fieldInfo = {
-                    type: event.target.tagName,
-                    value: event.target.value,
-                    action: event.type, // 'keydown', 'keyup', 'click', 'focus', 'change', etc.
-                };
-                // check if it is a login button
+            // if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+            //     const fieldInfo = {
+            //         type: event.target.tagName,
+            //         value: event.target.value,
+            //         action: event.type, // 'keydown', 'keyup', 'click', 'focus', 'change', etc.
+            //     };
+            //     // check if it is a login button
 
-                console.log("Field information:", fieldInfo);
+            //     console.log("Field information:", fieldInfo);
 
-                // Send message to background script with field information
-                // browser.runtime.sendMessage({ action: "form_interaction", payload: fieldInfo }).then((response) => {
-                //     console.log("Response from background script:", response);
-                // });
-            }
+            //     // Send message to background script with field information
+            //     // browser.runtime.sendMessage({ action: "form_interaction", payload: fieldInfo }).then((response) => {
+            //     //     console.log("Response from background script:", response);
+            //     // });
+            // }
         };
 
         document.addEventListener("keydown", handleUserInteraction);
